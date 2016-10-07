49ee742cca606d90da8f165602fab8ba31ee302b
ajayv12196 <ajayv12196@gmail.com>
2016-10-08 01:10:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,3 +42,32 @@ button.onclick =function() {
 
 //submit name
 
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit =document.getElementById("submit");
+
+submit.onclick=function(){
+    
+    //make request to the server and send this name
+    
+    //capture a list if names and render it in the list
+    
+    var names =['name1','name2','name3','name4'];
+    
+    var list="";
+    
+    for(var i=0; i<=names.length;i++){
+        
+        list= '<li>' +name[i]+'</li>';
+        
+    }
+    
+    var ul=document.getElementById("namelist");
+    
+    ul.innerHTML=list;
+    
+    
+    
+}
+
+
