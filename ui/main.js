f7387e1ef8e41b5c4471fc1eb7755de9cd388ea3
ajayv12196 <ajayv12196@gmail.com>
2016-10-08 00:33:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,35 @@ var button=document.getElementById("counter");
 var counter =0;
 button.onclick =function() {
     
-    counter=counter+1;
+    //create a request object(AJAX)
     
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
+    var request =new XMLHttpRequest();
+    
+    
+    // create the response and store in variable
+    request.onreadystatechange =function(){
+        
+        if(request.readystate === XMLHttpRequest.DONE ){
+            //TAKE SOME ACTION
+            
+            if(request.status === 200){
+                
+                var counter = request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+                
+                
+            }
+            
+        }
+        //not done yet
+    };
+    
+    //make the reques
+    
+    request.open('GET', 'http://ajayv12196.imad.hasura-app.io/counter', true);
+    request.send(null);
+
+    
     
 };
