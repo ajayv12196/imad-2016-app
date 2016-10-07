a99c81e0bc3f10a97264f9b53ad24b3eeebb2725
ajayv12196 <ajayv12196@gmail.com>
2016-10-08 01:46:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,6 +51,24 @@ app.get('/ui/aj.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'aj.jpg'));
 });
 
+var names[];
+
+app.get('/submit-name/:name', function (req, res) {
+    
+    //get the name from the request
+     
+    var name = req.params.name;
+    
+    names.push(name);
+    /*as the names is an object and we cant send it directly as the server accepts string 
+    here comes JSON(javascript orientation notation) in the picture 
+    jSO converts the js object to strings*/
+    res.send(JSON.stringfy(names));
+     
+})
+
+
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
