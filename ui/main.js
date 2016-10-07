5e6ffda912607091cadf5d2dba2ea9b53700ce0e
ajayv12196 <ajayv12196@gmail.com>
2016-10-08 01:12:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,7 +58,7 @@ submit.onclick=function(){
     
     for(var i=0; i<=names.length;i++){
         
-        list= '<li>' +name[i]+'</li>';
+        list= '<li>' +names[i]+'</li>';
         
     }
     
